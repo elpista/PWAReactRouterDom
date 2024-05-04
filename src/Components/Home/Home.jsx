c1c776b6cf21028c2ec522a2069a14311381ebc9
elpista <espadachin2003@gmail.com>
2024-05-04 15:49:12 -0300

    Home y app avanzados

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,1 +1,17 @@
-let algo;
+import Cards from "./Components/Cards/Cards.jsx"
+import Search from "./Components/Search/Search.jsx"
+import Header from "./Components/Header/Header.jsx"
+import Footer from "./Components/Footer/Footer.jsx"
+
+function Home(){
+    return(
+        <div className="Home">
+            <Header/>
+            <Search/>
+            <Cards />
+            <Footer/>
+        </div>
+    )
+}
+
+export default Home;
