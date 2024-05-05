7ec6b761527ad1a7de19307ee9709df078871d12
elpista <espadachin2003@gmail.com>
2024-05-05 19:01:53 -0300

    Detail.jsx casi terminado

---
+++ src/Components/Detail/Detail.jsx
@@ -0,0 +1,41 @@
+import Header from "../Header/Header.jsx"
+import Footer from "../Footer/Footer.jsx"
+
+// CAMBIAR POR FETCH
+let juego = {
+    nombre: "Fallout 4",
+    foto: "",
+    descripcion: "Bethesda Game Studios, el galardonado creador de Fallout 3 y The Elder Scrolls V: Skyrim, os da la bienvenida al mundo de Fallout 4, su juego más ambicioso hasta la fecha y la siguiente generación de mundos abiertos.",
+    precio: "$12 USD",
+    rating: "2.5",
+    id: "1"
+}
+
+function Detail(){
+
+return (
+    <div className="detail">
+        <Header/>
+        <div className="nombre">
+            <h2>{juego.nombre}</h2>
+        </div>
+        <div className="foto">
+            <img src={juego.foto}/>
+        </div>
+        <div className="descripcion">
+            <p>{juego.descripcion}</p>
+        </div>
+        <div className="Panel derecha">
+            <div className="precio">
+                <p>{juego.precio}</p>
+            </div>
+            <div className="rating">
+                <p>{juego.rating}</p>
+            </div>
+        </div>
+        <Footer/>
+    </div>
+)
+}
+
+export default Detail
