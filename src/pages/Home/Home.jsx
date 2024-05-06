28eabb9fea6a60ff02a70bb688eeaf199636a2ad
elpista <espadachin2003@gmail.com>
2024-05-06 17:38:14 -0300

    cambios a Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,28 @@
-import Cards from "../../Components/Cards/Cards.jsx"
-import Search from "../../Components/Search/Search.jsx"
-import Header from "../../Components/Header/Header.jsx"
-import Footer from "../../Components/Footer/Footer.jsx"
+import Cards from "../Cards/Cards.jsx"
+import Search from "../Search/Search.jsx"
+import Header from "../Header/Header.jsx"
+import Footer from "../Footer/Footer.jsx"
+import { useEffect, useState } from "react";
 
 function Home(){
+
+    const [Juegos, setJuegos] = useState([]);
+
+    const fetchJuegos = async () => {
+        const response = await fetch("/mocks/Juegos.json");
+        const result = await response.json;
+        setJuegos(result);
+    }
+
+    useEffect(() => {
+        fetchJuegos();
+    }, [])
+
     return(
         <div className="Home">
             <Header/>
             <Search/>
-            <Cards />
+            <Cards Juegos={Juegos}/>
             <Footer/>
         </div>
     )
